30bd608cb46e396e2b3911d0ef0962be01a571f1
Gabriel Ferreira <gabrielf.04.2002@gmail.com>
2025-04-17 21:18:10 -0300

    Upload da estrtutura do projeto

---
+++ src/main/java/com/gabrielferreira02/CraqueDaPartida/CraqueDaPartidaApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielferreira02.CraqueDaPartida;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CraqueDaPartidaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CraqueDaPartidaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabrielferreira02/CraqueDaPartida/CraqueDaPartidaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielferreira02.CraqueDaPartida;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CraqueDaPartidaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
