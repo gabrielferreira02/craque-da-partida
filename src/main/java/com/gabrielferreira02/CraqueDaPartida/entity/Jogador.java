c9961a9dbd695d247bc2b80cf6669196866762d2
Gabriel Ferreira <gabrielf.04.2002@gmail.com>
2025-04-21 10:30:54 -0300

    refactor: adicionado anotação table nas entidades e removido classpath no application.properties para script.sql

--- src/main/java/com/gabrielferreira02/CraqueDaPartida/entity/Jogador.java
+++ src/main/java/com/gabrielferreira02/CraqueDaPartida/entity/Jogador.java
@@ -1,15 +1,13 @@
 package com.gabrielferreira02.CraqueDaPartida.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
+@Table(name = "jogador")
 @Getter
 @Setter
 @AllArgsConstructor
